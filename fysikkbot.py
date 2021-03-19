2576d99eac75e14ca464279c20ab87f7b9ba6327
JennStro <jennys1994@gmail.com>
2021-03-19 22:25:40 +0100

    log

--- fysikkbot.py
+++ fysikkbot.py
@@ -3,31 +3,35 @@ import os
 import random
 import emoji
 from boto.s3.connection import S3Connection
+import sys
 
-token = S3Connection(os.environ['TOKEN'])
+if __name__ == "__main__":
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    sys.stdout.flush()
+    token = S3Connection(os.environ['TOKEN'])
 
-motivations = ["Keep going!", "Just do it!", "You're not as dumb as you think.", "Life will get better.", "Today is your day", "Heiaheia", "I'm sending you lots of hugs!", "You have so many other good qualities.", "Go kick some ass!", "Always look on the bright side of life.", "Don't limit yourself!", "Be yourself, everyone else is already taken!", "You are smart, strong and beautiful!", "It's your time to shine!!"]
+    client = discord.Client()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(client))
+        sys.stdout.flush()
 
-    if (message.content.startswith('hjelp') or message.content.startswith('Hjelp')):
-        random_motivation = random.choice(motivations)
-        await message.channel.send('Hello, @' + message.author.name + "! " + random_motivation + " \N{SLIGHTLY SMILING FACE}")
+    motivations = ["Keep going!", "Just do it!", "You're not as dumb as you think.", "Life will get better.", "Today is your day", "Heiaheia", "I'm sending you lots of hugs!", "You have so many other good qualities.", "Go kick some ass!", "Always look on the bright side of life.", "Don't limit yourself!", "Be yourself, everyone else is already taken!", "You are smart, strong and beautiful!", "It's your time to shine!!"]
 
-    if message.content.startswith('Ai'):
-      await message.channel.send("Aiiaiiiaiii. Men det fikser seg @" + message.author.name + " " + emoji.emojize(':thumbs_up:'))
-    
-    if (message.content == "hei" or message.content == "Hei" or message.content == "hei!" or message.content == "Hei!"):
-      await message.channel.send("Heiii " + message.author.name + "!!")
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-client.run(token)
+        if (message.content.startswith('hjelp') or message.content.startswith('Hjelp')):
+            random_motivation = random.choice(motivations)
+            await message.channel.send('Hello, @' + message.author.name + "! " + random_motivation + " \N{SLIGHTLY SMILING FACE}")
+
+        if message.content.startswith('Ai'):
+        await message.channel.send("Aiiaiiiaiii. Men det fikser seg @" + message.author.name + " " + emoji.emojize(':thumbs_up:'))
+        
+        if (message.content == "hei" or message.content == "Hei" or message.content == "hei!" or message.content == "Hei!"):
+        await message.channel.send("Heiii " + message.author.name + "!!")
+
+    client.run(token)
